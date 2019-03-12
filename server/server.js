35a420801da883e168030e72919a101487391ced
lnpastinante <lnpastinante@gmail.com>
2019-03-11 21:34:48 -0300

    fix server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import { join } from 'path'
-import express from 'express'
+const path = require('path')
+const express = require('express')
 
 const app = express()
 
-const publicPath = join(__dirname, '../public')
+const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath))
